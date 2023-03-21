e026110b5b9718d0c3d3e8823f928ce4ea4c9cfb
PravinK <pravin.kakde003@gmail.com>
2023-03-22 02:41:51 +0530

    Commit Changes

--- app/src/main/java/com/pravin/myweather/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pravin/myweather/ui/activity/MainActivity.kt
@@ -2,49 +2,55 @@ package com.pravin.myweather.ui.activity
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.gson.Gson
 import com.pravin.myweather.R
+import com.pravin.myweather.api.NetworkResult
 import com.pravin.myweather.databinding.ActivityMainBinding
-import com.pravin.myweather.ui.fragment.DashboardFragment
+import com.pravin.myweather.ui.viewmodel.DashboardViewModel
 import com.pravin.myweather.utils.isLocationEnabled
-import com.pravin.myweather.utils.showToast
+import com.pravin.myweather.utils.positiveButtonClick
+import com.pravin.myweather.utils.showAlertDialog
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
-
     private lateinit var binding: ActivityMainBinding
+    lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private val dashboardViewModel: DashboardViewModel by viewModels()
+    var PERMISSION_ALL = 1
+    var PERMISSIONS_ARRAY = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        supportFragmentManager
-            .beginTransaction()
-            .addToBackStack("ItemFragment")
-            .add(R.id.frameLayout, DashboardFragment())
-            .commit()
-
         requestLocationPermission()
-        showToast("Please turn on" + " your location...")
+//        dashboardViewModel.getWeatherData("18.5204", "73.8567")
+        collects()
     }
+
 
     private fun requestLocationPermission() {
         locationPermissionRequest.launch(
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            )
+            PERMISSIONS_ARRAY
         )
     }
 
@@ -53,37 +59,36 @@ class MainActivity : AppCompatActivity() {
     ) { permissions ->
         when {
             permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                Log.e("TAG", "ACCESS_FINE_LOCATION")
                 checkGPS()
             }
             permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                Log.e("TAG", "ACCESS_COARSE_LOCATION")
                 checkGPS()
             }
             else -> {
-                Log.e("TAG", "No location access granted")
+                Log.e("TAGG", "Permission denied")
             }
         }
     }
 
-
     private fun checkGPS() {
-        if (this.isLocationEnabled()) {
+        if (isLocationEnabled()) {
             getLocation()
         } else {
-            showToast("Please turn on" + " your location...")
-            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-            startActivity(intent)
+            showAlertDialog {
+                setTitle(context.resources.getString(R.string.important))
+                setMessage(context.resources.getString(R.string.enable_gps_message))
+                positiveButtonClick(context.resources.getString(R.string.ok)) {
+                    it.dismiss()
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                }
+            }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        checkGPS()
-    }
-
     fun getLocation() {
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
         mFusedLocationProviderClient.getCurrentLocation(
             Priority.PRIORITY_HIGH_ACCURACY,
             CancellationTokenSource().token
@@ -105,6 +110,49 @@ class MainActivity : AppCompatActivity() {
             val message = e.message
             val title = "Location fetching exception"
 //                    Common.AlertDialogBox(this, title, message)
+        }
+    }
+
+    private fun collects() {
+        lifecycleScope.launchWhenCreated {
+            dashboardViewModel.weatherResponseLiveData.observe(this@MainActivity) { responseData ->
+//                progressDialog.hide()
+                when (responseData) {
+                    is NetworkResult.Loading -> {
+//                        progressDialog.show()
+                    }
+                    is NetworkResult.Error -> {
+                        showAlertDialog {
+                            setTitle(context.resources.getString(R.string.error))
+                            setMessage(responseData.errorMessage?.message)
+                            positiveButtonClick(context.resources.getString(R.string.ok)) { }
+                        }
+                    }
+                    is NetworkResult.Success -> {
+                        responseData.data?.let {
+                            val apiResponse = responseData.data
+                            if (null != apiResponse) {
+
+                                Log.e("TAGG", Gson().toJson(apiResponse))
+//                                if (apiResponse.data.isNotEmpty()) {
+//                                    val preparedList =
+//                                        daySalesReconViewModel.getPreparedItemList(apiResponse.data)
+//                                    dayReconRecyclerViewAdapter.items = preparedList
+//                                } else {
+//                                    binding.withDataLayout.visibility = View.GONE
+//                                    binding.noDataLayout.visibility = View.VISIBLE
+//                                }
+                            } else {
+                                showAlertDialog {
+                                    setTitle(context.resources.getString(R.string.error))
+                                    setMessage(resources.getString(R.string.generic_error_message))
+                                    positiveButtonClick(context.resources.getString(R.string.ok)) { }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/pravin/myweather/utils/AppUtils.kt
+++ app/src/main/java/com/pravin/myweather/utils/AppUtils.kt
@@ -1,9 +1,12 @@
 package com.pravin.myweather.utils
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 fun Activity.isLocationEnabled(): Boolean {
     val locationManager = getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
@@ -15,4 +18,10 @@ fun Activity.isLocationEnabled(): Boolean {
 fun Activity.showToast(textToDisplay: String) {
     if (textToDisplay.isBlank()) return
     Toast.makeText(this, textToDisplay, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.requestPermissions(request: ActivityResultLauncher<Array<String>>, permissions: Array<String>) = request.launch(permissions)
+
+fun Activity.isAllPermissionsGranted(permissions: Array<String>) = permissions.all {
+    ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
 }
