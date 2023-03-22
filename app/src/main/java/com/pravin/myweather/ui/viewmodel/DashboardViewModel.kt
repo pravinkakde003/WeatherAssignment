60249d60f58af6e8a61382ec80d635213c88bc41
PravinK <pravin.kakde003@gmail.com>
2023-03-22 22:28:42 +0530

    Display Current weather completed

--- app/src/main/java/com/pravin/myweather/ui/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/pravin/myweather/ui/viewmodel/DashboardViewModel.kt
@@ -22,6 +22,10 @@ class DashboardViewModel @Inject constructor(private val weatherRepository: Weat
     val currentDate = MutableLiveData<String>()
     val weatherDescription = MutableLiveData<String>()
     val weatherMinMax = MutableLiveData<String>()
+    val feelsLike = MutableLiveData<String>()
+    val pressure = MutableLiveData<String>()
+    val humidity = MutableLiveData<String>()
+    val visibility = MutableLiveData<String>()
 
     val weatherResponseLiveData: LiveData<NetworkResult<CurrentWeatherResponse>>
         get() = weatherRepository.weatherResponseLiveData
@@ -50,7 +54,24 @@ class DashboardViewModel @Inject constructor(private val weatherRepository: Weat
         if (apiResponse.main.temp_min.toString()
                 .isNotBlank() && apiResponse.main.temp_max.toString().isNotBlank()
         ) {
-            weatherMinMax.value = "Min: "+apiResponse.main.temp_min.toString() + " | Max: "+apiResponse.main.temp_max.toString()
+            weatherMinMax.value =
+                "Min: " + apiResponse.main.temp_min.toString() + " | Max: " + apiResponse.main.temp_max.toString()
+        }
+
+        if (apiResponse.main.feels_like.toString().isNotBlank()) {
+            feelsLike.value = apiResponse.main.feels_like.toString()
+        }
+
+        if (apiResponse.main.pressure.toString().isNotBlank()) {
+            pressure.value = apiResponse.main.pressure.toString()
+        }
+
+        if (apiResponse.main.humidity.toString().isNotBlank()) {
+            humidity.value = apiResponse.main.humidity.toString()
+        }
+
+        if (apiResponse.visibility.toString().isNotBlank()) {
+            visibility.value = apiResponse.visibility.toString()
         }
     }
 }
